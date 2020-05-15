f3ccb49f6b537d5ca3ff2b66a4d54a7107fca1e6
Jamie Gardner <jamiedanielgrdner@gmail.com>
2020-05-15 13:31:33 +0100

    Display flashed messages

--- app/routes.py
+++ app/routes.py
@@ -22,13 +22,18 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+# Accept both GET and POST requests, not just the default GET
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # The following will only be true when the submit button is clicked, not when the page is loaded.
+    # If the form is not valid, based on the validation attributes, the form will be re-rendered to the user.
     if form.validate_on_submit():
+        # Flash a message to the user, used temporary until user auth is set up
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
+        # Redirect the user to the home page
         return redirect('/index')
 
     return render_template('login.html', title='Sign in', form=form)
