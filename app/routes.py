1f40978afe03bd657b05c1d58f8614c18dd16833
Jamie Gardner <jamiedanielgrdner@gmail.com>
2020-05-15 13:16:25 +0100

    Add login option

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 # The following lines are "decorators" which modify the following function
 # These associate the "/" and "/index" URLs with this function
@@ -20,3 +21,8 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
